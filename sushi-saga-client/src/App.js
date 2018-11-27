123ca32d031d924768c482b43532c7dc7221c1f4
alexk89 <kozhushko.vasyl@gmail.com>
2018-11-27 13:24:56 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,69 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+    state = {
+        balance: 100,
+        page: 0,
+        sushi: [],
+        eaten: []
+    };
 
-  render() {
-    return (
-      <div className="app">
-        <SushiContainer  />
-        <Table />
-      </div>
-    );
-  }
+    fetchData = async (url) => {
+        return (url) ? await fetch(url)
+            .then(resp => resp.json()) : []
+    };
+
+    async componentDidMount() {
+        const sushi = await this.fetchData(API);
+        sushi.forEach(item => {
+           item.clicked = false
+        });
+        this.setState({sushi})
+    }
+
+    paginationHandler = () => {
+        this.setState({page: this.state.page + 1})
+    };
+
+    clickOnSushiTable = (plate) => {
+        const newBalance = this.state.balance - plate.price;
+
+        if (newBalance >= 0) {
+            const eaten = [...this.state.eaten];
+            eaten.push(plate);
+
+            const statesSushi = [...this.state.sushi];
+            const clickedSushi = statesSushi.find(item => item.id === plate.id);
+
+            clickedSushi.clicked = true;
+
+            this.setState({
+                eaten,
+                balance: this.state.balance - plate.price,
+                sushi: [...statesSushi]
+            })
+        }
+
+    };
+
+    render() {
+        const sushiToRender = this.state.sushi.slice(this.state.page * 4, this.state.page * 4 + 4);
+
+        return (
+            <div className="app">
+                <SushiContainer
+                    sushiToRender={sushiToRender}
+                    paginationHandler={this.paginationHandler}
+                    clickOnSushiTable={this.clickOnSushiTable}/>
+                <Table balance={this.state.balance} eaten={this.state.eaten}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,29 @@
-import React, { Fragment } from 'react'
+import React, {Fragment} from 'react'
 import MoreButton from '../components/MoreButton'
 
 const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+    return (
+        <Fragment>
+            <div className="belt">
+                {
+                    props.sushiToRender.map(sushiItem => {
+                        const clickedClass = (sushiItem.clicked) ? "clicked" : "";
+
+                        return (
+                            <div className={`stack ${clickedClass}`} key={sushiItem.id}>
+                                {
+                                    (!sushiItem.clicked) && <img className={"sushi"} src={sushiItem.img_url} onClick={() => props.clickOnSushiTable(sushiItem)}/>
+                                }
+                                <div className={"plate"}></div>
+                                <h5 className={"sushi-details"}>{sushiItem.name} - {sushiItem.price}$</h5>
+                            </div>
+                        )
+                    })
+                }
+                <MoreButton paginationHandler={props.paginationHandler}/>
+            </div>
+        </Fragment>
+    )
 }
 
 export default SushiContainer
